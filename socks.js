2783c6c589d1c018a9be098f67ba3780bb98c317
jaspersorriso <s10149149@connect.np.edu.sg>
2015-10-03 02:01:46 +0800

    Added try catch block for socket.connect

--- socks.js
+++ socks.js
@@ -2,7 +2,13 @@ var io = require('socket.io')(8080);
 
 io.on('connection', function (socket) {
 
-	socket.join(socket.query.outlet);
+	try{
+		socket.join(socket.query.outlet);
+	}catch(e){
+		console.log(e);
+		console.log("Outlet not defined");
+	}
+	
 	console.log(socket.rooms);
 
 });
