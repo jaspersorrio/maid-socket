0cfa323d0c794aabce4121992799a33f0a60c348
jaspersorriso <s10149149@connect.np.edu.sg>
2015-10-03 02:07:45 +0800

    Checking handshake

--- socks.js
+++ socks.js
@@ -3,12 +3,13 @@ var io = require('socket.io')(8080);
 io.on('connection', function (socket) {
 
 	try{
-		socket.join(socket.query.outlet);
+		// socket.join(socket.handshake.header.outlet);
 	}catch(e){
 		console.log(e);
 		console.log("Outlet not defined");
 	}
 	console.log(socket);
+	console.log(socket.handshake);
 	console.log(socket.rooms);
 
 });
