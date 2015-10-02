13092e39488b0a034d74f333622a70b9d3e514c3
jaspersorriso <s10149149@connect.np.edu.sg>
2015-10-03 02:09:34 +0800

    Found correct json path to query

--- socks.js
+++ socks.js
@@ -3,12 +3,12 @@ var io = require('socket.io')(8080);
 io.on('connection', function (socket) {
 
 	try{
-		// socket.join(socket.handshake.header.outlet);
+		socket.join(socket.handshake.headers.query.outlet);
 	}catch(e){
 		console.log(e);
 		console.log("Outlet not defined");
 	}
-	console.log(socket);
+	// console.log(socket);
 	console.log(socket.handshake);
 	console.log(socket.rooms);
 
