eeb276ce8ce435ab0e89c591facc8763f9c59911
jaspersorriso <s10149149@connect.np.edu.sg>
2015-10-03 02:14:41 +0800

    Added success event

--- socks.js
+++ socks.js
@@ -4,6 +4,7 @@ io.on('connection', function (socket) {
 
 	try{
 		socket.join(socket.handshake.query.outlet);
+		io.to(socket.handshake.query.outlet).emit('success', "User have connected"):
 	}catch(e){
 		console.log(e);
 		console.log("Outlet not defined");
