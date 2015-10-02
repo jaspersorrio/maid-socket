fc23c5abe56aedb245ab0c3b042c91e2a9493c8e
jaspersorriso <s10149149@connect.np.edu.sg>
2015-10-03 02:11:44 +0800

    Found the path to query

--- socks.js
+++ socks.js
@@ -3,7 +3,7 @@ var io = require('socket.io')(8080);
 io.on('connection', function (socket) {
 
 	try{
-		socket.join(socket.handshake.headers.query.outlet);
+		socket.join(socket.handshake.query.outlet);
 	}catch(e){
 		console.log(e);
 		console.log("Outlet not defined");
